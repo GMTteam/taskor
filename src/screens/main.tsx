999c47964e3790123c7296249e78961f05e67bdb
nguyenanhtuVMO <105730255+nguyenanhtuVMO@users.noreply.github.com>
2024-04-05 12:06:42 +0700

    Animate the checkbox

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,10 +1,16 @@
-import * as React from 'react';
+import React, { useCallback, useState } from 'react';
 import { Text, useColorMode } from 'native-base';
+import { Pressable } from 'react-native';
 import { Center, Box, VStack, useColorModeValue } from 'native-base';
 import ThemeToggle from '../components/theme-toggle';
 import AnimatedCheckbox from '../components/animated-checkbok';
 
 export default function MainScreen() {
+    const [checked, setChecked] = useState(false);
+    const handlePressCheckbox = useCallback(() => {
+        setChecked(prev => !prev);
+    }, []);
+
     const { colorMode } = useColorMode();
 
     return (
@@ -16,7 +22,9 @@ export default function MainScreen() {
         >
             <VStack space={5} alignItems="center">
                 <Box w="100px" h="100px">
-                    <AnimatedCheckbox />
+                    <Pressable onPress={handlePressCheckbox}>
+                        <AnimatedCheckbox checked={checked} />
+                    </Pressable>
                 </Box>
                 <Box p={10} bg={useColorModeValue('red.500', 'yellow.500')}>
                     <Text color={colorMode === 'dark' ? 'white' : 'black'}>
